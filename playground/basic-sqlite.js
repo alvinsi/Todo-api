e3c5d8fbe5183dd15884f4c6c4a23b967dc2ba64
alvinleolopolisa@gmail.com <alvinleolopolisa@gmail.com>
2015-10-10 16:28:28 -0400

    added todo model and POST /todos

--- playground/basic-sqlite.js
+++ playground/basic-sqlite.js
@@ -10,14 +10,14 @@ var Todo = sequelize.define('todo', {
 		allowNull: false,
 		validate:{
 			notEmpty: true
-		}
+		} 
 	},
 	completed: {
 		type: Sequelize.BOOLEAN,
 		allowNull: false,
 		defaultValue: false
 	}
-})
+});
 
 sequelize.sync({force: true}).then(function () {
 	console.log('Everything is synced');
